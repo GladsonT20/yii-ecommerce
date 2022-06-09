c1f3ce25b7ca111bb9076a82b5c97145d93f0a4a
Gladson Ongeza <sibamakambu@gmail.com>
2022-06-09 09:42:00 +0300

    remove test full stop

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -316,7 +316,7 @@ AppAsset::register($this);
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?.</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
                 <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
